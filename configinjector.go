07aa11aa1473d5885abf7a2fdb60d09f7b0ba5f6
Robert Rong <robert.rong@agile-solutions.ch>
2019-09-28 10:08:45 +0200

    f

--- configinjector.go
+++ configinjector.go
@@ -1,3 +1,169 @@
+The Go Playground   Imports 
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
+60
+61
+62
+63
+64
+65
+66
+67
+68
+69
+70
+71
+72
+73
+74
+75
+76
+77
+78
+79
+80
+81
+82
+83
+84
+85
+86
+87
+88
+89
+90
+91
+92
+93
+94
+95
+96
+97
+98
+99
+100
+101
+102
+103
+104
+105
+106
+107
+108
+109
+110
+111
+112
+113
+114
+115
+116
+117
+118
+119
+120
+121
+122
+123
+124
+125
+126
+127
+128
+129
+130
+131
+132
+133
+134
+135
+136
+137
+138
+139
+140
+141
+142
+143
+144
+145
+146
+147
+148
+149
+150
+151
+152
+153
+154
+155
+156
+157
+158
+159
+160
+161
+162
+163
+164
+165
 package main
 
 import (
@@ -23,7 +189,7 @@ func main() {
 
 	exitCode := 0
 
-	param1 := flag.String("url", "https://github.com/o7planning/spring-cloud-config-git-repo-example/blob/master/app-about-company.properties", "Application yaml file.")
+	param1 := flag.String("url", "http://spring.config.com:8888/cms/default/PRD", "Application yaml file.")
 
 	param2 := flag.String("directory", "", "Directory to download configs")
 
@@ -32,27 +198,28 @@ func main() {
 	uri := *param1
 	dir := *param2
 
-	fmt.Printf("Application config url : %s downloaded at : %s\n", uri, dir)
-
-	fmt.Printf("DownloadToFile From: %s.\n", uri)
+	fmt.Printf("Application config : %s processed at : %s\n", uri, dir)
 
 	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+	// HTTP pull spring boot config from Spring Cloud Config server
+	//if response, err := http.Get(uri); err == nil {
+	fmt.Printf("downloaded %s.\n", uri)
 
-	if response, err := http.Get(uri); err == nil {
-		fmt.Printf("downloaded %s.\n", uri)
+	//defer response.Body.Close()
 
-		defer response.Body.Close()
-
-	nbody,_ := ioutil.ReadAll(response.Body)
+	//nbody, _ := ioutil.ReadAll(response.Body)
 
 	var m interface{}
 
-	error := json.Unmarshal([]byte(nbody), &m)
+	error := json.Unmarshal([]byte(mock), &m)
 	if error != nil {
 		panic(error)
 	}
-
-	f, ferror := os.Create("application.properties")
+	/*
+	* write a new property file aggregating content of all spring boot yaml files
+	* This file gets pulled in on a k8s configmap GO template on a HELM chart
+	*/
+	f, ferror := os.Create(dir + "/application.properties")
 	if ferror != nil {
 		panic(ferror)
 	}
@@ -60,7 +227,7 @@ func main() {
 	w := bufio.NewWriter(f)
 
 	mu := m.(map[string]interface{})
-
+	// parse out all property key value pairs
 	for k, v := range mu {
 
 		switch vv := v.(type) {
@@ -80,14 +247,15 @@ func main() {
 					for k, z := range source {
 
 						if k == "source" {
-							fmt.Println("properties found")
+							//fmt.Println("properties found")
 							values := z.(map[string]interface{})
-
+							// write propery key value pair to new property file on k8s configmap
 							for propertyName, propertyValue := range values {
+								fmt.Fprintln(w, propertyName, "=", propertyValue)
 								fmt.Println(propertyName, "=", propertyValue)
-								fmt.Fprintf(w, "%s\n", propertyName)
 								w.Flush()
 							}
+
 						}
 
 					}
@@ -100,9 +268,9 @@ func main() {
 
 	}
 
-	} else {
-		panic(err)
-	}
+	//} else {
+	//	panic(err)
+	//}
 
 	os.Exit(exitCode)
 }
@@ -124,39 +292,43 @@ func HTTPDownload(uri string) ([]byte, error) {
 
 const mock = `
 
-{
-
-   "name":"irs",
-
-   "profiles":[
-
+{  
+   "name":"cms",
+   "profiles":[  
       "default"
-
    ],
-
    "label":"PRD",
-
-   "version":"0f8c9d17e9435b2072fab97356944e3155ad22b5",
-
+   "version":"0322baa10d3f1d1a9452dcaab16ec6b95fb8225c",
    "state":null,
-
-   "propertySources":[
-
-      {
-
-         "name":"https://mycomp.com/scm/sfs/config.git/irs.yml",
-
-         "source":{
-
-            "AAAAAA":9999,
-            "XXXXXX":"1.4"
-
+   "propertySources":[  
+      {  
+         "name":"https://cloud.config.com/scm/id/config.git/cms.yml",
+         "source":{  
+            "server.tomcat.max-http-header-size":131072,
+            "server.max-http-header-size":10000000,
+            "spring.http.multipart.max-file-size":"20MB",
+            "spring.http.multipart.max-request-size":"20MB",
+            "local.server.port":8780,
+            "datasource.jdbc.url":"jdbc:oracle:thin:www.cms.com:9999:cms001",
+            "datasource.jdbc.driverClassName":"oracle.jdbc.xa.client.OracleXADataSource",
+            "datasource.jdbc.userName":"xxx",
+            "datasource.jdbc.password":"xxx",
+            "datasource.pool.initialSize":2,
+            "datasource.pool.maxIdle":5,
+            "datasource.pool.maxActive":20,
+            "activeDirectory.url":"ldap://xxxx.compute.amazonaws.com:389",
+            "activeDirectory.domain":"xxx.net",
+            "activeDirectory.search-filter":"((&objectClass=user)(userPrincipalName={0})(memberof=CN=Users,DC=pub,DC=net))",
+            "email.smtp.host":"smtppublic.mail.com",
+            "email.smtp.port":25,
+            "email.smtp.username":"xxx",
+            "email.smtp.password":"test",
+            "application.security.jwt.secret":"lksadfuhsdaflöweuuus",
+            "application.security.jwt.validityMinutes":60,
+            "application.security.allowedOrigins[0]":"http://localhost:4200"
          }
-
       }
-
    ]
-
 }
-
 `
+
